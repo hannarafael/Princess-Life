7910e9f7caea490c63923478d6f82d4fc46cbc14
hanna rafael <hannanrafael@gmail.com>
2019-12-14 21:40:50 -0500

    login routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,26 +2,35 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+ 
+ 
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
+      res.json("example", {
         example: dbExample
       });
     });
   });
 
+
+  app.post("/login", function(req, res) {
+     
+      res.json("200", {
+        example: req.body
+      });
+  });
+
+  app.post("/search", function(req, res) {
+     
+    res.json("200", {
+      example: req.body
+    });
+});
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.json("404");
   });
 };
