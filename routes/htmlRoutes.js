77c8903ca921f05cbde28d5cd3eee8576322b46d
hanna rafael <hannanrafael@gmail.com>
2019-12-15 21:58:17 -0500

    fixed api

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,18 @@
 var db = require("../models");
+const axios = require("axios");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
- 
- 
+
+
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.json("example", {
         example: dbExample
       });
@@ -15,40 +20,46 @@ module.exports = function(app) {
   });
 
 
-  app.post("/login", function(req, res) {
-     
-      res.json("200", {
-        example: req.body
-      });
-  });
+  app.post("/login", function (req, res) {
 
-  app.post("/search", function(req, res) {
-     
     res.json("200", {
       example: req.body
     });
-});
-
-
-app.post("/signup", function(req, res) {
-
-  axios.get('https://api.yelp.com/v3/businesses/search?term=food&location=philadelphia', {
-        headers: {
-            Authorization: "Bearer 0XmJFcySOehBbfOmxvf1H63RKMdvpEJsvTNpomcB9O4QnuuUkQbAh8Nxy9HZ88xfGJ3xOXuypueX1qjo4mdVJfmHioHkHfczT0b3evJAUkSBhqhinqHk1erdbNjrXXYx"
-        }
-    }).then((response) => {
-            console.log(response.data.businesses);
-     
-  res.json("200", {
-    example: req.body
   });
-});
 
+  app.post("/signup", function (req, res) {
+
+    res.json("200", {
+      example: req.body
+    });
+  });
+
+ 
+  app.post("/search", function (req, res) {
+    search = 'https://api.yelp.com/v3/businesses/search?term=food&location=city';
+    food = search.replace(/food/,req.body.query);
+    location= food.replace(/city/,req.body.city);
+    console.log(location);
+    axios.get(location, {
+      headers: {
+        Authorization: "Bearer 0XmJFcySOehBbfOmxvf1H63RKMdvpEJsvTNpomcB9O4QnuuUkQbAh8Nxy9HZ88xfGJ3xOXuypueX1qjo4mdVJfmHioHkHfczT0b3evJAUkSBhqhinqHk1erdbNjrXXYx"
+      }
+    }).then((response) => {
+      console.log(response.data.businesses);
+
+      res.json("200", {
+        example: req.body,
+        query: req.body.query,
+        data: response.data.businesses
+      }); 
+    });
+
+
+
+  });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.json("404");
   });
-});
-
 };
--- server.js
+++ server.js
@@ -31,7 +31,7 @@ db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
+      PORT, 
       PORT
     );
   });
